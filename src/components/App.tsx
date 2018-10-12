be276125ab35ae61ffa4a91da467a2a1c39e07c6
Robin Law <Robin-Law@users.noreply.github.com>
2018-10-12 12:42:08 -0700

    initial setup

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './../assets/css/App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Robin Law</h1>
+        </header>
+        <p className="App-intro">
+          Hi there.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
